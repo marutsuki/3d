d61a3ce89e9f671fd79aa62bb4cdccbe7f320d3e
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-06-04 18:40:49 +1000

    Added playground sub project

---
+++ playground/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <h1>
+    <span class="welcome"> </span>
+
+    Hello
+  </h1>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
+
+  h1 {
+    width: 100%;
+  }
+</style>
